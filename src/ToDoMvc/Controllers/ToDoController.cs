377ea522730b05f9ed8f61bc8329179285eb5451
opet <opet@Academico.edu>
2018-09-27 20:53:18 -0300

    adicionou edit

---
+++ src/ToDoMvc/Controllers/ToDoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoMvc.Models.View;
+using ToDoMvc.Services;
+using ToDoMvc.Models;
+
+
+namespace ToDoMvc.Controllers
+{
+    public class ToDoController : Controller
+    {
+
+        private readonly IToDoItemService _toDoItemService;
+
+        public ToDoController(IToDoItemService service)
+        {
+            _toDoItemService = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //busca items de algum lugar
+            //se necessario criar uma view model
+            //encaminha para view
+            var vm = new ToDoViewModel
+            {
+                Items = await _toDoItemService.GetIncompleteItemAsync()
+            };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> AddItem(NewToDoItem newItem)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var succesfull = await _toDoItemService.AddItemAsync(newItem);
+
+            if (!succesfull)
+                return BadRequest(new { error = "Could not add item" });
+            return Ok();
+        }
+
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+
+            if (id == Guid.Empty) return BadRequest();
+
+            var succesfull = await _toDoItemService
+                .MarkDoneAsync(id);
+
+            if (!succesfull)
+                return BadRequest(
+                    new { error = "Could not mark item as done" });
+            return Ok();
+        }
+
+    }
+}
---
+++ src/ToDoMvc/wwwroot/js/site.js
@@ -0,0 +1,53 @@
+﻿// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#add-item-button')
+        .on('click', addItem);
+    $('.done-checkbox').on('click', markDone);
+});
+
+var postError = (function () {
+    var $itemError = $('#add-item-error');
+    function erroOnPost(data) {
+        var error = data.statusText;
+        if (data.responseJSON) {
+            var key = Object.keys(data.responseJSON)[0]
+            error = data.responseJSON[key];
+        }
+        $itemError.text(error).show();
+    }
+    return {
+        hide: () => $itemError.hide(),
+        onError: errorOnPost
+    }
+})();
+
+function addItem() {
+    var newTitle = $('#add-item-title');
+    var newDueAt = $('#add-item-due-at');
+    return function () {
+        postError.hide();
+        $.post(
+            '/ToDo/AddItem',
+            {
+                title: $newTitle.val(),
+                dueAt: $newDuet.val()
+            },
+            () => window.location = '/ToDo'
+        ).fail(postError.onError);
+    };
+}
+function markDone(ev) {
+    // ev.target === checkbox
+    ev.target.disabled = true;
+    postError.hide();
+    $.post('/ToDo/MarkDone',
+        { id: ev.target.name },
+        function () {
+            var row = ev.target
+                .parentElement //td
+                .parentElement //tr
+            row.classlist.add('done');
+        }
+    ).fail(postError.onError);
+}
